6542c61ca510c891aa669f6568337bfa26322d19
Fumiya Sakai <just1factory@gmail.com>
2019-10-21 16:31:39 +0900

    第1章: サンプル追加対応

---
+++ 01_SearchMediaSampleByRxSwift/SearchMediaSampleByRxSwift/SearchMediaSampleByRxSwift/ViewController/Main/ArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleViewController.swift
+//  SearchMediaSampleByRxSwift
+//
+//  Created by 酒井文也 on 2019/10/14.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 01_SearchMediaSampleByRxSwift/SearchMediaSampleByRxSwift/SearchMediaSampleByRxSwift/ViewController/Main/GalleryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryViewController.swift
+//  SearchMediaSampleByRxSwift
+//
+//  Created by 酒井文也 on 2019/10/14.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

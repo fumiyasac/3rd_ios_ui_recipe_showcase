9609bd11dc59b91818893d6fb490d8e6e9a6673b
Fumiya Sakai <just1factory@gmail.com>
2020-10-29 00:14:46 +0900

    第1章: Realm/APIに関するクラスのファイルだけを追加

---
+++ 01_LayeredArchitectureAndUiExample/LayeredArchitectureAndUiExample/LayeredArchitectureAndUiExample/Infrastructure/APIManager/APIRequestManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIRequestManager.swift
+//  LayeredArchitectureAndUiExample
+//
+//  Created by 酒井文也 on 2020/10/29.
+//
+
+import Foundation
+import RxSwift
+
+// MARK: - Protocol
+
+// ※ Mockに置き換えられるような形にしておくのがポイント
+
+protocol APIRequestProtocol {}

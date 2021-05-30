9d36207f25df2aba994f265d704ec09059795323
Fumiya Sakai <just1factory@gmail.com>
2021-05-30 19:34:49 +0900

    [第1章サンプルコード] 簡易的DI機構作成＆Group整理

---
+++ 01_RxSwiftAndUiExample/RxSwiftAndUiExample/RxSwiftAndUiExample/Dependencies/DependeciesContainer.swift
@@ -0,0 +1,55 @@
+//
+//  DependeciesContainer.swift
+//  RxSwiftAndUiExample
+//
+//  Created by 酒井文也 on 2021/05/30.
+//
+
+import Foundation
+
+// MEMO: こちらのDI実装を元に改変を加えた形
+// https://www.fabrizioduroni.it/2020/04/19/dependecy-injection-swift.html
+
+final class DependeciesContainer {
+
+    // MARK: - DIコンテナ自体はSingletonとして保持する
+    static let shared = DependeciesContainer()
+
+    private init() {}
+
+    private var dependecies: [DependencyKey: Any] = [:]
+
+    func register<T>(_ type: T.Type, impl: Any, name: String? = nil) {
+        let dependencyKey = DependencyKey(type: type, name: name)
+        dependecies[dependencyKey] = impl
+    }
+
+    func resolve<T>(_ type: T.Type, name: String? = nil) -> T {
+        let dependencyKey = DependencyKey(type: type, name: name)
+        if let dep = dependecies[dependencyKey] as? T {
+            return dep
+        } else {
+            let protocolTypeName = NSString(string: "\(type)").components(separatedBy: ".").last!
+            fatalError("\(protocolTypeName)の依存性を解決できませんでした。当該実装クラス:\(protocolTypeName).")
+        }
+    }
+}
+
+final class DependencyKey: Hashable, Equatable {
+    private let type: Any.Type
+    private let name: String?
+
+    init(type: Any.Type, name: String? = nil) {
+        self.type = type
+        self.name = name
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(type))
+        hasher.combine(name)
+    }
+
+    static func == (lhs: DependencyKey, rhs: DependencyKey) -> Bool {
+        return lhs.type == rhs.type && lhs.name == rhs.name
+    }
+}
---
+++ 01_RxSwiftAndUiExample/RxSwiftAndUiExample/RxSwiftAndUiExample/Dependencies/DependenciesDefinition.swift
@@ -0,0 +1,48 @@
+//
+//  DependenciesDefinition.swift
+//  RxSwiftAndUiExample
+//
+//  Created by 酒井文也 on 2021/05/30.
+//
+
+import Foundation
+import RxSwift
+
+final class DependenciesDefinition {
+
+    // MARK: - Function
+
+    func inject() {
+
+        // MEMO: 同じProtocolを適合させるがLocalに保持したデータを利用したい際につける名前
+        let local = "local"
+
+        // MEMO: 処理を実行する際にバックグラウンドスレッドにしたい際につける名前
+        let background = "background"
+
+        // MEMO: インスタンスを保持するための場所
+        let dependecies = DependeciesContainer.shared
+
+        // MARK: - Scheduler
+
+        // メインスレッド動作
+        dependecies.register(
+            ImmediateSchedulerType.self,
+            impl: MainScheduler.instance
+        )
+        // バックグラウンドスレッド動作
+        dependecies.register(
+            ImmediateSchedulerType.self,
+            impl: SerialDispatchQueueScheduler(qos: .default),
+            name: background
+        )
+
+        // MARK: - Infra
+
+        // MARK: - Repository
+
+        // MARK: - Scheduler
+
+        // MARK: - Presenter
+    }
+}

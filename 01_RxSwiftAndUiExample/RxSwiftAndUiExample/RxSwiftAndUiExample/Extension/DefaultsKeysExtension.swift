982ae083d0842b4605b53c1679001e919440c9f7
Fumiya Sakai <just1factory@gmail.com>
2021-06-01 22:56:46 +0900

    [第1章サンプルコード] SwiftyUserDefaultsによるUserDefault管理

---
+++ 01_RxSwiftAndUiExample/RxSwiftAndUiExample/RxSwiftAndUiExample/Extension/DefaultsKeysExtension.swift
@@ -0,0 +1,26 @@
+//
+//  DefaultsKeysExtension.swift
+//  RxSwiftAndUiExample
+//
+//  Created by 酒井文也 on 2021/06/01.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+// MEMO: ライブラリ「SwiftyUserDefaults」を利用して管理する
+
+extension DefaultsKeys {
+
+    // MARK: - Property
+
+    // MEMO: チュートリアル画面を終了しているか
+    var isTutorialFinished: DefaultsKey<Bool> {
+        .init("isTutorialFinished", defaultValue: false)
+    }
+
+    // MEMO: アンケート入力画面を終了しているか
+    var isQuestionnaireFinished: DefaultsKey<Bool> {
+        .init("isQuestionnaireFinished", defaultValue: false)
+    }
+}

c929028f060a5a5adc383d3de29d95aaa3a5d686
Fumiya Sakai <just1factory@gmail.com>
2021-06-12 18:12:14 +0900

    [第1章サンプルコード] 細かな部分の追加＆追記

--- 01_RxSwiftAndUiExample/RxSwiftAndUiExample/RxSwiftAndUiExample/ViewController/RootViewController.swift
+++ 01_RxSwiftAndUiExample/RxSwiftAndUiExample/RxSwiftAndUiExample/ViewController/RootViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 final class RootViewController: UIViewController {
 
+    // MARK: - Properties
+
+//    // MEMO: Storyboard初期化時にDIすることで反映する形（iOS13からの新機能）
+//    var presenter: RootPresenter
+
     // MARK: - Override
 
     override func viewDidLoad() {
